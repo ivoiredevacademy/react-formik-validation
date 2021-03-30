80cfdd24ee437f911c11d9661f042ad77c39fd98
Juvenal Pengele <juvenal.pengele@gmail.com>
2021-03-30 10:58:06 +0000

    Add a style to the form validation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import Sidebar from "./components/Sidebar"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row">
+        <div className="col-lg-5">
+          <Sidebar/>
+        </div>
+        <div className="col-lg-7">
+          <div class="row form-container">
+            <div className="col-md-12 col-lg-7 mx-auto">
+            <h1>Register</h1>
+            <form>
+              <div class="form-group">
+                <label htmlFor="name">Nom</label>
+                <input class="form-control" />
+              </div>
+              <div class="form-group">
+                <label htmlFor="name">Email</label>
+                <input class="form-control" />
+              </div>
+              <div class="form-group">
+                <label htmlFor="name">Numéro de téléphone</label>
+                <input class="form-control" />
+              </div>
+              <div class="form-group">
+                <label htmlFor="name">Mot de passe</label>
+                <input class="form-control" />
+              </div>
+              <div class="form-group">
+                <label htmlFor="name">Mot de passe (confirmation)</label>
+                <input class="form-control" />
+              </div>
+              <div class="form-group">
+                <label htmlFor="name">Mot de passe (confirmation)</label>
+                <input class="form-control" />
+              </div>
+              <div class="custom-control custom-checkbox">
+                <input type="checkbox" className="custom-control-input" id="customSwitch1" checked="" />
+                <label className="custom-control-label" for="customSwitch1">J'accepte <a href="#" _target="blanl">les conditions d'utilisations</a></label>
+              </div>
+              <div className="form-group mt-4">
+                <button className="btn btn-light-primary px-4">Créer mon compte</button>
+              </div>
+            </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Sidebar() {
+    return (
+        <div class="sidebar">
+            <div>
+                <h2 class="text-white">Ameliorez l'image de votre entreprise dès aujourd'hui.</h2>
+                <p class="text-white">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+                <img src="/img/startup.png"/>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
