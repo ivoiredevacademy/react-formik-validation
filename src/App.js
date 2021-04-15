aa240aa0169ff4a2b6dbbe12d4d95e860b410bc0
Juvenal Pengele <juvenal.pengele@gmail.com>
2021-04-15 09:59:01 +0000

    Integrate the Formik component instead of useFormik hook

--- src/App.js
+++ src/App.js
@@ -1,49 +1,34 @@
 import Aside from "./components/Aside";
-import { useFormik } from "formik";
-import * as yup from "yup";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
-const validationSchema = yup.object({
-  name: yup.string()
-          .required("Le nom est obligatoire"),
-  email: yup.string()
-          .required('L\'adresse e-mail est obligatoire')
-          .email("Veuillez saisir une adresse e-mail validate"),
-  password: yup.string()
-              .required("Le mot de passe est obligatoire")
-              .min(8),
-  passwordConfirmation: yup.string()
-        .required("La confirmation du mot de passe est obligatoire")
-        .oneOf([yup.ref('password')], "Les mots de passe ne correspondent pas"),
-  phoneNumber: yup.number("Veuillez entrer un numero de téléphone valide")
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required("Le nom est obligatoire"),
+  email: Yup.string().required("L'adresse e-mail est obligatoire")
+          .email("Veuillez une adresse e-mail valide"),
+  password: Yup.string().required("Le mot de passe est obligatoire")
+              .min(6, "Le mot de passe doit avoir 6 caractères"),
+  passwordConfirmation: Yup.string().oneOf([Yup.ref("password")], "Les mots de passe ne correspondent pas"),
+  phoneNumber: Yup.number().typeError("Veuillez entrer un numero de téléphoe valide")
                 .required("Le numero de téléphone est obligatoire"),
-  gcu: yup.boolean()
-          .oneOf([true], "Veuillez accepter les conditions d'utilisation")        
+  gcu: Yup.boolean().oneOf([true], "Veuillez accepter les conditions d'utilisation")
 })
 
 
 function App() {
-  const initialValues =   {
+  const initialValues =  {
     name: "",
     email: "",
+    phoneNumber: "",
     password: "",
     passwordConfirmation: "",
-    phoneNumber: "",
     gcu: false
+  };
+
+  function handleSubmit(formValues) {
+    console.log("Form values", formValues)
   }
-
-  function createUser(values) {
-    console.log("Form submitted... OK");
-  }
-
-  const formik = useFormik({
-    initialValues,
-    onSubmit: createUser,
-    validationSchema
-  });
-
-  const { name, email, phoneNumber, gcu, passwordConfirmation, password} = formik.values;
-  const { handleChange, handleBlur } = formik
-
+  
 
   return (
     <div className="App container-fluid">
@@ -54,66 +39,78 @@ function App() {
         <div className="col-lg-7">
           <div className="row form-container">
             <div className="col-md-12 col-lg-7 mx-auto">
-            <h1>Inscription</h1>
-            <form onSubmit={formik.handleSubmit}>
-              <div className="form-group">
-                <label htmlFor="name">Nom</label>
-                <input className="form-control" id="name" name="name" type="text" 
-                  { ...formik.getFieldProps('name')}
-                />
+              <h1>Inscription</h1>
+              <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
                 {
-                  formik.errors.name && formik.touched.name && <span className="text-danger">{ formik.errors.name }</span>
+                  formik => (
+                    <form onSubmit={formik.handleSubmit}>
+                    <div className="form-group">
+                      <label htmlFor="name">Nom</label>
+                      <input className="form-control" id="name" name="name" type="text"
+                        { ...formik.getFieldProps("name") }
+                      />
+                      {
+                        formik.errors.name && formik.touched.name &&
+                        <span className="text-danger">{formik.errors.name}</span>
+                      }
+                    </div> 
+                    <div className="form-group">
+                      <label htmlFor="email">Email</label>
+                      <input className="form-control"id="email" name="email" type="email"
+                      { ...formik.getFieldProps("email") }
+                      />
+                      {
+                        formik.errors.email && formik.touched.email &&
+                        <span className="text-danger">{formik.errors.email}</span>
+                      }
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="phoneNumber">Numéro de téléphone</label>
+                      <input className="form-control" type="text" name="phoneNumber" id="phoneNumber"
+                      { ...formik.getFieldProps("phoneNumber") }
+                      />
+                      {
+                        formik.errors.phoneNumber && formik.touched.phoneNumber &&
+                        <span className="text-danger">{formik.errors.phoneNumber}</span>
+                      }
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="password">Mot de passe</label>
+                      <input className="form-control" type="password" name="password" id="password"
+                        { ...formik.getFieldProps("password") }
+                      />
+                      {
+                        formik.errors.password && formik.touched.password &&
+                        <span className="text-danger">{formik.errors.password}</span>
+                      }
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="passwordConfirmation">Mot de passe (confirmation)</label>
+                      <input className="form-control" type="password" name="passwordConfirmation" id="passwordConfirmation"
+                      { ...formik.getFieldProps("passwordConfirmation") }
+                      />
+                      {
+                        formik.errors.passwordConfirmation && formik.touched.passwordConfirmation &&
+                        <span className="text-danger">{formik.errors.passwordConfirmation}</span>
+                      }
+                    </div>
+                    <div className="custom-control custom-checkbox">
+                      <input type="checkbox" className="custom-control-input" id="gcu"
+                        { ...formik.getFieldProps({ name: "gcu", checked: false }) }
+                      />
+                      <label className="custom-control-label" htmlFor="gcu">J'accepte <a href="#" _target="blank">les conditions d'utilisation</a></label>
+                      {
+                        formik.errors.gcu && formik.touched.gcu &&
+                        <span className="text-danger d-block">{formik.errors.gcu}</span>
+                      }
+                    </div>
+                    <div className="form-group mt-4">
+                      <button className="btn btn-light-primary px-4">Créer mon compte</button>
+                    </div>
+                  </form>
+                  )
                 }
-              </div> 
-              <div className="form-group">
-                <label htmlFor="name">Email</label>
-                <input className="form-control"id="email" name="email" type="email"
-                  { ...formik.getFieldProps('email')}
-                />
-                {
-                  formik.errors.email && formik.touched.email && <span className="text-danger">{ formik.errors.email }</span>
-                }
-              </div>
-              <div className="form-group">
-                <label htmlFor="name">Numéro de téléphone</label>
-                <input className="form-control" type="text" name="phoneNumber"
-                  { ...formik.getFieldProps('phoneNumber')}
-                />
-                 {
-                  formik.errors.phoneNumber && formik.touched.phoneNumber && <span className="text-danger">{ formik.errors.phoneNumber }</span>
-                }
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Mot de passe</label>
-                <input className="form-control" type="password" name="password"
-                  { ...formik.getFieldProps('password')}
-                />
-                {
-                  formik.errors.password && formik.touched.password && <span className="text-danger">{ formik.errors.password }</span>
-                }
-              </div>
-              <div className="form-group">
-                <label htmlFor="name">Mot de passe (confirmation)</label>
-                <input className="form-control" type="password" name="passwordConfirmation"
-                  { ...formik.getFieldProps('passwordConfirmation')}
-                />
-                 {
-                  formik.errors.passwordConfirmation && formik.touched.passwordConfirmation && <span className="text-danger">{ formik.errors.passwordConfirmation }</span>
-                }
-              </div>
-              <div className="custom-control custom-checkbox">
-                <input type="checkbox" className="custom-control-input" id="gcu" 
-                  {...formik.getFieldProps({ name: "gcu", checked: false })}
-                />
-                <label className="custom-control-label" htmlFor="gcu">J'accepte <a href="#" _target="blank">les conditions d'utilisation</a></label>
-                {
-                  formik.errors.gcu && formik.touched.gcu && <span className="text-danger d-block">{ formik.errors.gcu }</span>
-                }
-              </div>
-              <div className="form-group mt-4">
-                <button className="btn btn-light-primary px-4">Créer mon compte</button>
-              </div>
-            </form>
+              </Formik>
             </div>
           </div>
         </div>
